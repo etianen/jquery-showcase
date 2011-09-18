4861b96ccb52fdf4f5a078b495fe96105f2db07f
Dave Hall <dave@etianen.com>
2011-09-18 17:26:46 +0100

    Adding in image preload functionality.

--- src/showcase/jquery.showcase.js
+++ src/showcase/jquery.showcase.js
@@ -38,6 +38,33 @@
             left: (containerWidth - imageWidth) / 2
         });
     }
+    
+    /**
+     * Applies a preloader to the given image.
+     *
+     * Great for avoiding a jerky introduction to the page.
+     */
+    function preload(image, delay, callback) {
+        var readyToDisplay = false;
+        var loaded = false;
+        function doCallback() {
+            if (readyToDisplay && loaded) {
+                callback();
+            }
+        }
+        // Set the delay function.
+        setTimeout(function() {
+            readyToDisplay = true;
+            doCallback();
+        }, delay);
+        // This little dance ensures that the load event is fired even
+        // if the image is in the browser cache.
+        var src = image.attr("src");
+        image.attr("src", "").load(function() {
+            loaded = true;
+            doCallback();
+        }).attr("src", src);
+    }
 
     /**
      * The showcase plugin.
@@ -59,40 +86,45 @@
         // Run the plugin.
         return this.each(function() {
             // Find and activate the showcase.
-            var showcase = $(this).addClass("jquery-showcase");
+            var showcase = $(this).addClass("jquery-showcase").show();
             var thumbnails = $("<div/>", {
                 "class": "jquery-showcase-thumbnails"
             });
             showcase.append(thumbnails);
             // Configure each slide.
             var slides = showcase.find(settings.slideSelector);
-            slides.each(function() {
+            slides.each(function(n) {
                 var slide = $(this).addClass("jquery-showcase-slide");
                 var header = slide.find(settings.headerSelector).addClass("jquery-showcase-header");
                 var title = slide.find(settings.titleSelector).addClass("jquery-showcase-title");
                 var subTitle = slide.find(settings.subTitleSelector).addClass("jquery-showcase-subtitle");
-                // Create the slide image.
                 var image = slide.find(settings.imageSelector).addClass("jquery-showcase-image");
-                var src = image.attr("src");
-                function scaleImage() {
-                    fitToContainer(slide, image);
-                }
-                scaleImage();
-                $(window).resize(scaleImage);
+                // Preload the image.
+                preload(image, 0, function() {
+                    image.fadeIn();
+                });
                 // Create the slide thumbnail.
                 var thumbnail = $("<div/>", {
                     "class": "jquery-showcase-thumbnail"
                 });
                 thumbnails.append(thumbnail);
                 var thumbnailImage = $("<img>", {
-                    src: src,
+                    src: image.attr("src"),
                     width: image.width(),
                     height: image.height()
                 });
-                // Apply aspect ratio preserving downsize.
-                fitToContainer(thumbnail, thumbnailImage);
-                
                 thumbnail.append(thumbnailImage);
+                // Preload the thumbnail image.
+                preload(thumbnailImage, n * 200, function() {
+                    thumbnailImage.fadeIn();
+                });
+                // Set the image scaling callbacks.
+                function scaleImages() {
+                    fitToContainer(slide, image);
+                    fitToContainer(thumbnail, thumbnailImage);
+                }
+                scaleImages();
+                $(window).resize(scaleImages);
             });
             // Only show the first slide.
             slides.slice(1).hide();
